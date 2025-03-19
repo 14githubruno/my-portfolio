c343ef83a9d4a5e3eb398a57ea4f20154d41061c
bruno <bruno.lusardi.96@gmail.com>
2025-03-19 09:17:19 +0100

    refact comment

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ import { routing } from "@/i18n/routing";
 /**
  * This page renders when a route like `/unknown.txt` is requested.
  *
- * In this case, the layout at `app/[locale]/layout.tsx` receives
+ * The layout at `app/[locale]/layout.tsx` receives
  * an invalid value as the `[locale]` param and calls `notFound()`.
  */
 export default function NotFound() {
