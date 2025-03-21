41850ef27c9626c4204f91597e92ded869b55158
bruno <bruno.lusardi.96@gmail.com>
2025-03-21 16:19:20 +0100

    refact external-link to receive optional text and target props + add default target prop

--- src/components/external-link.tsx
+++ src/components/external-link.tsx
@@ -3,21 +3,24 @@ import { ReactNode } from "react";
 
 export type ExternalLinkProps = {
   href: string;
-  text: string;
+  target?: "_blank" | "_self";
+  text?: string;
   children?: ReactNode;
 };
 
 export default function ExternalLink({
   href,
+  target = "_blank",
   text,
   children,
 }: ExternalLinkProps) {
+  if (!text && !children) return null;
   return (
     <a
       className={clsx("text-sm underline", "lg:text-base")}
       href={href}
-      target="_blank"
-      rel="noopener noreferrer"
+      target={target}
+      rel={target === "_blank" ? "noopener noreferrer" : undefined}
     >
       {children || text}
     </a>
