f9e76f7ae45ccac0fde959b77eed97d68e6e939b
bruno <bruno.lusardi.96@gmail.com>
2025-03-29 15:37:40 +0100

    set max-width of paragraphs container (about-section.tsx)

--- src/components/sections/about-section.tsx
+++ src/components/sections/about-section.tsx
@@ -23,7 +23,7 @@ export default function AboutSection() {
       >
         {title}
       </h3>
-      <div className="flex flex-col gap-4">
+      <div className={clsx("flex flex-col gap-4", "lg:w-3/4")}>
         {paragraphs.map((paragraph: string) => {
           return (
             <p className="text-zinc-800 dark:text-amber-50" key={paragraph}>
