075fe0ee196146af63697466999c705193c2b646
bruno <bruno.lusardi.96@gmail.com>
2025-04-01 14:55:03 +0200

    refact error.tsx

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import clsx from "clsx";
 import { useTranslations } from "next-intl";
 import { useEffect } from "react";
+import BaseParagraph from "@/components/base-paragraph";
 
 type Props = {
   error: Error;
@@ -16,9 +18,19 @@ export default function Error({ error, reset }: Props) {
   const t = useTranslations("Error");
 
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <button onClick={() => reset()}>{t("buttonText")}</button>
+    <div
+      className={clsx(
+        "flex flex-col gap-4 border-b border-dashed border-pink-500 py-2 pl-2 dark:border-zinc-700",
+        "lg:py-4 lg:pl-4"
+      )}
+    >
+      <BaseParagraph paragraph={t("paragraph")} />
+      <button
+        onClick={() => reset()}
+        className="w-fit border border-dashed border-zinc-800 bg-amber-50 p-1.5 text-xs dark:border-amber-50 dark:bg-pink-900"
+      >
+        {t("buttonText")}
+      </button>
     </div>
   );
 }
