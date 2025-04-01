0fdf5abbbaec2c3a8f5c8e8257e10ef7d6229fda
bruno <bruno.lusardi.96@gmail.com>
2025-04-01 15:25:23 +0200

    store viewport max-width in a variable

--- src/components/global/page-wrapper.tsx
+++ src/components/global/page-wrapper.tsx
@@ -9,7 +9,7 @@ export default function PageWrapper({ children }: Props) {
   return (
     <main
       className={clsx(
-        "mx-auto my-0 flex w-full max-w-[90rem] flex-col gap-8",
+        "mx-auto my-0 flex w-full max-w-[var(--max-viewport-width)] flex-col gap-8",
         "border-t border-r border-l border-dashed border-pink-500 dark:border-zinc-700"
       )}
     >
