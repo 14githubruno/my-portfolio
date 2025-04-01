6a770609eefb69d9410aa805982ee1ceee3bd7f1
bruno <bruno.lusardi.96@gmail.com>
2025-04-01 13:45:39 +0200

    use base-paragraph in not-found-page.tsx

--- src/components/global/not-found-page.tsx
+++ src/components/global/not-found-page.tsx
@@ -1,6 +1,12 @@
 import { useTranslations } from "next-intl";
+import BaseParagraph from "../base-paragraph";
 
 export default function NotFoundPage() {
   const t = useTranslations("NotFoundPage");
-  return <p>{t("paragraph")}</p>;
+  return (
+    <BaseParagraph
+      paragraph={t("paragraph")}
+      extraTWClasses="border-b border-dashed border-pink-500 dark:border-zinc-700 pl-2 lg:pl-4"
+    />
+  );
 }
